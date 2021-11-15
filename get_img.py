4ab1cc879fee35a88e7398f72ea39c8262a4fa62
Michał Miotk <michalmiotk95@gmail.com>
2021-11-15 19:51:57 +0100

    poprawiono blad z brakiem dekodowania

--- get_img.py
+++ get_img.py
@@ -4,9 +4,9 @@ from bs4 import BeautifulSoup
 def is_match_exactly_this_class(bs4tag):
     return True if len(bs4tag['class']) == 1 else False
 
-def get_all_car_imgs_relative_url(relative_path):
+def get_all_car_imgs_urls(*, relative_path):
     r = requests.get('https://mklr.pl'+relative_path)
-    return get_all_car_imgs_relative_url(str(r.content))
+    return get_all_car_imgs_from_html(r.content.decode("utf-8"))
 
 def get_all_car_imgs_from_html(html_text):
     soup = BeautifulSoup(html_text, features="html.parser")
@@ -18,13 +18,12 @@ def get_all_car_imgs_from_html(html_text):
             relative_urls.append(img_src)
     return relative_urls
 
-
 def get_one_img_url(page_relative_path):
-    car_imgs_relative_url = get_all_car_imgs_relative_url(page_relative_path)
+    car_imgs_relative_url = get_all_car_imgs_urls(relative_path=page_relative_path)
     car_relative_url = car_imgs_relative_url[0]
     car_img_full_url = 'https://mklr.pl' + car_relative_url
     return car_img_full_url
 
-    
+   
 if __name__=='__main__':
     print(get_one_img_url('/Mercedes-Benz-81-marka'))
