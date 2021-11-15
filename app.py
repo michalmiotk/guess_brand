a8d492db461de20cc8c05e0a6ab1006db47c8528
Michał Miotk <michalmiotk95@gmail.com>
2021-11-15 19:57:29 +0100

    dodano losowanie marki ktora sie pojawi

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main_page():
-    brand_url = list(brands.values())[0]
+    brand_url = random.choice(list(brands.values()))
     car_img_link = get_one_img_url(brand_url)
     return render_template('index.html', car_img_link=car_img_link)
 
