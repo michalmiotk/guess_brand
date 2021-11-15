cee05cfc4bcbe5afdbbad084b803c64826a2c700
Michał Miotk <michalmiotk95@gmail.com>
2021-11-15 19:14:34 +0100

    dodano dwa testy

--- test/test_get_all_car_imgs_from_html.py
+++ test/test_get_all_car_imgs_from_html.py
@@ -2,20 +2,66 @@ import pytest
 
 from get_img import get_all_car_imgs_from_html
 
-def test_get_all_car_imgs_get_all_car_imgs_from_html():
-    webpage = """
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-            </head>
-            <body>
-                <div class="pictureImage ">
-                    <a>
-                        <img src="tajne_dane" />
-                    </a>
-                </div>
-            </body>
-            </html>
-            """
-    
+
+
+@pytest.fixture
+def header():
+    return """
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+        </head>
+        """
+
+def test_get_all_car_imgs_from_html(header):
+    webpage = header + """
+        <body>
+            <div class="pictureImage ">
+                <a>
+                    <img src="tajne_dane" />
+                </a>
+            </div>
+        </body>
+        </html>
+        """
+
     assert get_all_car_imgs_from_html(webpage) == ["tajne_dane"]
+
+def test_get_all_car_imgs_from_html_When_two_images_are(header):
+    webpage = header + """
+        <body>
+            <div class="pictureImage ">
+                <a>
+                    <img src="tajny_adres1" />
+                </a>
+            </div>
+            <div class="pictureImage ">
+                <a>
+                    <img src="tajny_adres2" />
+                </a>
+            </div>
+        </body>
+        </html>
+        """
+
+    assert get_all_car_imgs_from_html(webpage) == ["tajny_adres1", 'tajny_adres2']
+
+
+def test_get_all_car_imgs_from_html_When_not_typical_images_are(header):
+    webpage = header + """
+        <body>
+            <img src="logo" />
+            <div class="pictureImage ">
+                <a>
+                    <img src="tajny_adres1" />
+                </a>
+            </div>
+            <div class="pictureImage ">
+                <a>
+                    <img src="tajny_adres2" />
+                </a>
+            </div>
+        </body>
+        </html>
+        """
+    assert get_all_car_imgs_from_html(webpage) == ["tajny_adres1", 'tajny_adres2']
