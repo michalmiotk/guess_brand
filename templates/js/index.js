3281a6c5e813e76d1934822ed1c866ea1923b420
Michał Miotk <michalmiotk95@gmail.com>
2021-11-19 13:06:20 -0800

    deleted console logs

--- templates/js/index.js
+++ templates/js/index.js
@@ -1,10 +1,7 @@
 function getOption() {
     let selectedValue = document.getElementById('greet').value;
-    console.log(selectedValue);
-    console.log("a powinno" + '{{ true_brand }}')
     let s= document.getElementById("result_text");
     if(selectedValue == '{{ true_brand }}'){
-        console.log("zwyciestwo");
         s.innerHTML = 'good answer';
     }else{
         s.innerHTML = 'bad answer';
